923f28e20bc535dd030c16a8913338dc58398880
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2022-11-29 16:35:08 +0000

    re-add the hardcoded parameters for gbc

--- config/fieldparams/minimal.go
+++ config/fieldparams/minimal.go
@@ -9,9 +9,6 @@ const (
 	RandaoMixesLength                     = 64            // EPOCHS_PER_HISTORICAL_VECTOR
 	HistoricalRootsLength                 = 16777216      // HISTORICAL_ROOTS_LIMIT
 	ValidatorRegistryLimit                = 1099511627776 // VALIDATOR_REGISTRY_LIMIT
-	Eth1DataVotesLength                   = 32            // SLOTS_PER_ETH1_VOTING_PERIOD
-	PreviousEpochAttestationsLength       = 1024          // MAX_ATTESTATIONS * SLOTS_PER_EPOCH
-	CurrentEpochAttestationsLength        = 1024          // MAX_ATTESTATIONS * SLOTS_PER_EPOCH
 	SlashingsLength                       = 64            // EPOCHS_PER_SLASHINGS_VECTOR
 	SyncCommitteeLength                   = 32            // SYNC_COMMITTEE_SIZE
 	RootLength                            = 32            // RootLength defines the byte length of a Merkle root.
